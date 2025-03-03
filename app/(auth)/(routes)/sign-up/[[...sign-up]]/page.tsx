67e8ad98d41e900ae37bdda480af864795e37ce8
mundane <799699348@qq.com>
2025-03-03 17:52:39 +0800

    接入clerk，navbar

---
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,15 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUp,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
+const SignUpPage = () => {
+  return <SignUp />;
+};
+
+export default SignUpPage;
