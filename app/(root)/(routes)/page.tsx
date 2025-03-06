c0f3d41f154bda56166007c1bab1441430e3da08
mundane <799699348@qq.com>
2025-03-06 22:37:15 +0800

    添加companion搜索和消息模型，完善主页数据查询逻辑

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -2,13 +2,37 @@ import SearchInput from "@/components/search-input";
 import { UserButton } from "@clerk/nextjs";
 import prismadb from "@/lib/prismadb";
 import { Categories } from "@/components/categories";
+import { Companions } from "@/components/companions";
 
-const RootPage = async () => {
+interface RootPageProps {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  };
+}
+
+const RootPage = async ({ searchParams }: RootPageProps) => {
+  const { categoryId, name } = await searchParams;
+  const data = await prismadb.companion.findMany({
+    where: {
+      categoryId: categoryId,
+      name: {
+        search: name,
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      _count: { select: { messages: true } },
+    },
+  });
   const categories = await prismadb.category.findMany();
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
       <Categories data={categories} />
+      <Companions data={data} />
     </div>
   );
 };
