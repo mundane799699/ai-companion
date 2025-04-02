2e8025cd7588e3c15b6c0f215d0f4965a15220c5
mundane <799699348@qq.com>
2025-04-02 15:00:53 +0800

    deployment

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -11,11 +11,16 @@ import { useProModal } from "@/hooks/use-pro-modal";
 import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 export const ProModal = () => {
   const proModal = useProModal();
   const [loading, setLoading] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   const onSubscribe = async () => {
     try {
       setLoading(true);
@@ -27,6 +32,9 @@ export const ProModal = () => {
       setLoading(false);
     }
   };
+  if (!isMounted) {
+    return null;
+  }
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
       <DialogContent>
